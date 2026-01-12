9ca3054429cbbfb4fc4522ff8cf992084f82c8b1
zycata <166348388+zycata@users.noreply.github.com>
2026-01-11 17:48:45 -0800

    made it a class (sorry function programming)

--- xlsx_to_ics/excel.py
+++ xlsx_to_ics/excel.py
@@ -4,44 +4,57 @@ import warnings
 # Filter out the specific openpyxl UserWarning
 warnings.filterwarnings("ignore", category=UserWarning, module='openpyxl')
 
-first_col_name: str = "start"
-cols_to_drop = [first_col_name, "drop", "swap", "credits", "grading basis", "instructional format", "delivery mode"]
+import pprint
 
-df: pd.DataFrame = pd.read_excel("View_My_Courses.xlsx", skiprows=2)
-df.columns = df.columns.str.lower()
-df.columns.values[0] = first_col_name
+class Workday_Schedule:
+    first_col_name: str = "start"
+    cols_to_drop = [first_col_name, "drop", "swap", "credits", "grading basis", "instructional format", "delivery mode"]
+    
+    def __init__(self, df: pd.DataFrame):
+        self.schedule_data = df
+        pass
+
+    @classmethod
+    def read_workday_excel(self, file_path:str, skip_rows: int):
+        df: pd.DataFrame = pd.read_excel(file_path, skiprows=skip_rows)
+        df.columns = df.columns.str.lower()
+        df.columns.values[0] = self.first_col_name
+        for col_name in self.cols_to_drop:
+            try:
+                df = df.drop(col_name, axis=1)
+            except KeyError:
+                print(col_name, "does not exist, skipping")
+        return self(df)
+
+    def clean_excel(self, df: pd.DataFrame = None):
+        # has to be a better way to do that right
+        if df is None:
+            df = self.schedule_data
+        # this code is so bad that ai could never
+        df["section"] = df["section"].apply(lambda s: s.split(" ")[1])
+        df = df[df['registration status'] == 'Registered']
+
+    def get_list_of_row(self, row_num: int) -> list:
+        dataframe = self.schedule_data
+        list = []
+        data_row = dataframe.iloc[row_num]
+        # print("-------print col info--------")
+        for name, datapoint in data_row.items():
+            # print(f"{name} :  {datapoint}")
+            if (datapoint is np.nan):
+                # print("is nan omg")
+                list.append("n/a")
+            else:
+                list.append(datapoint)
+        return list
 
 
 
-print(df.columns.values)
-for col_name in cols_to_drop:
-    try:
-        df = df.drop(col_name, axis=1)
-    except KeyError:
-        print(col_name, "does not exist, skipping")
 
 
-# df["section"] = df["section"].split(" ")[1]
-df["section"] = df["section"].apply(lambda s: s.split(" ")[1])
+workday_s = Workday_Schedule.read_workday_excel("View_My_Courses.xlsx", 2)
 
-print(df.columns.values)
+workday_s.clean_excel()
 
-print(df.head())
-
-df_filtered = df[df['registration status'] == 'Registered']
-
-def get_list_of_row(row_num, dataframe) -> list:
-    list = []
-    data_row = dataframe.iloc[row_num]
-    print("-------print col info--------")
-    for name, datapoint in data_row.items():
-        print(f"{name} :  {datapoint}")
-        if (datapoint is np.nan):
-            # print("is nan omg")
-            list.append("n/a")
-        else:
-            list.append(datapoint)
-    return list
-
-print(get_list_of_row(4, df))
+pprint.pprint(workday_s.get_list_of_row(4))
 

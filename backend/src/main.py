3dafb1a813515f11697a2d4b29e27e68013526b1
zycata <hyperconger@gmail.com>
2026-02-18 10:58:44 -0800

    backend folder restructure

---
+++ backend/src/main.py
@@ -0,0 +1,19 @@
+
+
+
+'''
+General Guide to what happens here
+
+receive excel file
+
+return err if bad
+
+convert excel to courseinfo data type
+
+use that to create .ics file
+
+return any errs if errors occur
+
+
+'''
+

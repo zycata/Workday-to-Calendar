b0ec5e2b6cc326fc4bd4d4377877d5e4842cb579
zycata <hyperconger@gmail.com>
2026-02-18 12:14:17 -0800

    tweaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,34 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const [loading, setLoading] = useState(false);
+  const [word, setWord] = useState("Click to start...");
+  
+  const fetchWord = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:5100/get-word");
+      const data = await response.json();
+      setWord(data.word);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setWord("Error :(");
+    }
+  };
 
+ 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={fetchWord} disabled={loading}>
+          {loading ? "Fetching..." : `Current word: ${word}`}
+        </button>
+        
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>

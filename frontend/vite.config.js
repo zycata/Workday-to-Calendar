94cd072c0306fdce6d6a6c344ae9b912320a88e3
zycata <hyperconger@gmail.com>
2026-02-19 15:40:32 -0800

    Fixed local setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5100,
+    strictPort: true, // This prevents Vite from jumping to 5101 if 5100 is busy
+  },
   plugins: [react()],
 })
